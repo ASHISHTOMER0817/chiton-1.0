e4e8fc543231b68dbe37898d6b20fecbc97125c2
ASHISHTOMER0817 <135249489+ASHISHTOMER0817@users.noreply.github.com>
2024-02-13 01:53:02 +0530

    Update fetchedData.tsx

--- src/app/components/fetchedData.tsx
+++ src/app/components/fetchedData.tsx
@@ -13,10 +13,7 @@ export default async function fetchedData(path: String, productcode: string, ind
       currentpage: index,
       pagesize: pagesize,
     },
-    headers: {
-      'X-RapidAPI-Key': '354341de18mshcc5b15c0fa6fd42p1947ebjsn7b6b759452b8',
-      'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
-    }
+   
   };
 
   try {
